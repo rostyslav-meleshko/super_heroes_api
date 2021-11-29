5dc49dab601e7e62c1157d1d6e8bb0e9ea579f78
Rostyslav Meleshko <meleskorostyslav@gmail.com>
2021-11-29 10:53:45 +0200

    " feat: create base layout
    add header, main, footer
    add input for search heroes in header
    main shows all heroes
    add pagination to footer
    "

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import type {} from "@mui/lab/themeAugmentation";
+import { heroData } from "./helpers/typesDef";
+import {
+  Grid,
+  Container,
+  useMediaQuery,
+  ImageList,
+  ImageListItem,
+  CircularProgress,
+  Input,
+} from "@material-ui/core";
+import Pagination from "@material-ui/lab/Pagination";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const isMobile = useMediaQuery("(max-width: 640px)");
+  const [allHeroes, setAllHeroes] = useState<heroData[] | []>([]);
+  const [isServerResponded, setIsServerResponded] = useState(false);
+  const [isServerError, setIsServerError] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const getHeroesFromServer = async () => {
+    await fetch("https://akabab.github.io/superhero-api/api/all.json")
+      .then((response) => response.json())
+      .then((result) => {
+        setAllHeroes(result);
+        setIsServerResponded(true);
+        setIsServerError(false);
+      })
+      .catch((error) => {
+        console.warn(error);
+        setIsServerResponded(true);
+        setIsServerError(true);
+      });
+  };
+
+  const heroesPerPage = useMemo(() => {
+    return isMobile ? 12 : 24;
+  }, [isMobile]);
+
+  const paginationPagesQuantity = useMemo(() => {
+    return Math.ceil(allHeroes.length / heroesPerPage);
+  }, [allHeroes, heroesPerPage]);
+
+  const handleSearchInput = () => {};
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
+
+  const paginatedHeroes = useMemo(() => {
+    const startIndex = (page - 1) * heroesPerPage;
+    const endIndex =
+      page > paginationPagesQuantity ? page : heroesPerPage * page;
+
+    console.log(111, startIndex, endIndex);
+    return [...allHeroes].slice(startIndex, endIndex);
+  }, [page, allHeroes, paginationPagesQuantity, heroesPerPage, isMobile]);
+
+  useEffect(() => {
+    getHeroesFromServer();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="stretch"
+      >
+        <header>
+          <Container maxWidth={isMobile ? "sm" : "lg"}>
+            SuperHeroes
+            <Input
+              placeholder="Enter hero name"
+              fullWidth={true}
+              color="primary"
+            />
+            {isMobile && <p> mobile </p>}
+          </Container>
+        </header>
+
+        {!isServerResponded && (
+          <Container>
+            <CircularProgress
+              size={200}
+              variant="indeterminate"
+              thickness={3}
+            />
+          </Container>
+        )}
+
+        {isServerError && (
+          <Container>
+            <p>Loading error. Reload page</p>
+          </Container>
+        )}
+
+        {isServerResponded && !isServerError && (
+          <main>
+            <Container maxWidth={isMobile ? "sm" : "lg"}>
+              Heroes list
+              {isMobile && <p> mobile </p>}
+              {paginatedHeroes.length > 0 && (
+                <ImageList cols={isMobile ? 3 : 6} gap={2} rowHeight={180}>
+                  {paginatedHeroes.map((hero) => (
+                    <ImageListItem key={hero.id}>
+                      <img
+                        className="hero_card"
+                        src={`${hero.images.md}`}
+                        alt={`${hero.name} avatar`}
+                        width="120"
+                      />
+                    </ImageListItem>
+                  ))}
+                </ImageList>
+              )}
+            </Container>
+          </main>
+        )}
+
+        <footer>
+          <Container maxWidth={isMobile ? "sm" : "lg"}>
+            SuperHeroes Pagination
+            <Pagination
+              count={paginationPagesQuantity}
+              size="small"
+              page={page}
+              onChange={handlePageChange}
+            />
+          </Container>
+        </footer>
+      </Grid>
     </div>
   );
-}
+};
 
 export default App;
