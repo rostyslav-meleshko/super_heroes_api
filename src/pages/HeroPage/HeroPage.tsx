68057a7e273b1a2edf5da2abd6993943cce3ca30
Rostyslav Meleshko <meleshkorostyslav@gmail.com>
2021-12-07 18:10:56 +0200

    feat: first commit

--- src/pages/HeroPage/HeroPage.tsx
+++ src/pages/HeroPage/HeroPage.tsx
@@ -1,10 +1,19 @@
 import { FC, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import {
+  Container,
+  useMediaQuery,
+  Grid,
+  useTheme,
+  Typography,
+} from "@material-ui/core";
 
 import { stateAllHeroes } from "store/selectors";
 
 const HeroPage: FC = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
   const location = useLocation();
   const { heroName, heroId } =
     useParams<{ heroName: string; heroId: string }>();
@@ -15,16 +24,34 @@ const HeroPage: FC = () => {
 
   useEffect(() => {}, []);
   return (
-    <>
-      {currentHero && (
-        <>
-          <h2>{currentHero?.name}</h2>
-          <img src={currentHero?.images.lg} alt={currentHero?.name} />
-        </>
-      )}
+    <Container>
+      <Typography variant={isMobile ? "h4" : "h2"} align="center">
+        {currentHero?.name}
+      </Typography>
+      <Grid container spacing={2} direction={isMobile ? "column" : "row"}>
+        {currentHero && (
+          <>
+            <Grid item xs={6}>
+              <img
+                src={
+                  isMobile
+                    ? `${currentHero.images.md}`
+                    : `${currentHero.images.lg}`
+                }
+                alt={currentHero?.name}
+              />
+            </Grid>
+            <Grid item xs={6}></Grid>
+          </>
+        )}
 
-      {!currentHero && <p>No such hero found</p>}
-    </>
+        {!currentHero && (
+          <Typography align="center" variant="h2">
+            No such hero found
+          </Typography>
+        )}
+      </Grid>
+    </Container>
   );
 };
 
